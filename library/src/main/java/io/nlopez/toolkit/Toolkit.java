4982005ddf4f837552793efee15c129f890ad8f6
Nacho L <nacho@nlopez.io>
2014-05-18 12:14:49 +0200

    First commit

---
+++ library/src/main/java/io/nlopez/toolkit/Toolkit.java
@@ -0,0 +1,17 @@
+package io.nlopez.toolkit;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Nacho Lopez on 28/10/13.
+ */
+public class Toolkit {
+
+    public static void main(String[] args) {
+        System.out.println("Does nothing :)");
+    }
+}
---
+++ sample/src/main/java/io/nlopez/toolkit/sample/application/MainActivity.java
@@ -0,0 +1,40 @@
+package io.nlopez.toolkit.sample.application;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import io.nlopez.toolkit;
+
+/**
+ * Created by Nacho Lopez on 28/10/13.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
