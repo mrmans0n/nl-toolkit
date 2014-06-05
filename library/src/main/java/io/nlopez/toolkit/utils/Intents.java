0688b5745ee9e26328c3c3d4c3a23184badcd750
Nacho L <nacho@nlopez.io>
2014-06-05 20:54:51 +0200

    Added intents for menial tasks in Intents class

---
+++ library/src/main/java/io/nlopez/toolkit/utils/Intents.java
@@ -0,0 +1,38 @@
+package io.nlopez.toolkit.utils;
+
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Created by mrm on 05/06/14.
+ */
+public class Intents {
+
+    private Intents() {
+    }
+
+    public static Intent newShareTextIntent(String text, String chooserText) {
+        return Intent.createChooser(newShareTextIntent(text), chooserText);
+    }
+
+    private static Intent newShareTextIntent(String text) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, text);
+        shareIntent.setType("text/*");
+        return shareIntent;
+    }
+
+    private static Intent newShareBinaryIntent(Uri uri, String mimeType, String chooserText) {
+        return Intent.createChooser(newShareBinaryIntent(uri, mimeType), chooserText);
+    }
+
+    private static Intent newShareBinaryIntent(Uri uri, String mimeType) {
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        shareIntent.setType(mimeType);
+        return shareIntent;
+    }
+
+}
